fc4f31b87dd126f76c975f33d0491b5288408ec6
mateusferreira <seraomateus@hotmail.com>
2017-03-10 15:48:13 -0300

    fourth commit for test - FlahLight

--- app/src/main/java/com/example/mateus_2/begintestsonandroid/MainActivity.java
+++ app/src/main/java/com/example/mateus_2/begintestsonandroid/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.mateus_2.begintestsonandroid;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+   Camera camera;
+    Camera.Parameters parameters;
+    boolean torchRequest = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)){
+            camera = Camera.open();
+            parameters = camera.getParameters();
+        }
+    }
+
+    public void turnLamp(View v) {
+
+        if(torchRequest == false){
+            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            camera.setParameters(parameters);
+            camera.startPreview();
+            torchRequest = true;
+        }
+
+        else{
+            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+            camera.setParameters(parameters);
+            camera.stopPreview();
+            torchRequest = false;
+        }
     }
 }
